23eab2a182171f8ee97ceb42878ece38385f306d
Donatos Sarras <donatos.sarras@oddschecker.com>
2021-09-06 19:21:45 +0300

    formatting

--- src/config.js
+++ src/config.js
@@ -1,3 +1,7 @@
-export const { MONGODB_URI, AUTH_SECRET, BCRYPT_SALT: saltString } = process.env;
+export const {
+  MONGODB_URI,
+  AUTH_SECRET,
+  BCRYPT_SALT: saltString,
+} = process.env;
 
 export const BCRYPT_SALT = Number(saltString);
